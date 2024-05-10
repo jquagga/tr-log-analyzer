78adb064712a1be32427378c5ae54d81e5e81304
Justin Settle <jus10@partlycloudy.org>
2024-05-10 09:41:46 -0400

    Additional refactoring

--- trlog.py
+++ trlog.py
@@ -13,30 +13,24 @@ import pandas as pd
 def parselog():
     calldict = {}
     logfile = gzip.open("tr.log.gz", "rt")
+    # Define the regex pattern for our log entries
+    log_pattern = r".*\[(\S+\s\S+)\]\s+\((\S+)\)\s+\[(\S+)\]\s+(\d+)\S+\s+\S+\s+(\d+).*Freq:\s+(\d+\.\d+).*MHz\s+(.*)"
     for line in logfile:
-        # Define the regex pattern for our log entries
-        log_pattern = r".*\[(\S+\s\S+)\]\s+\((\S+)\)\s+\[(\S+)\]\s+(\d+)\S+\s+\S+\s+(\d+).*Freq:\s+(\d+\.\d+).*MHz\s+(.*)"
-        # Example log entry
-        # line = "[2024-05-09 12:31:45.009426] (info)   [pwcp25]	126C	TG:       1007 (            PWPD West 1)	Freq: 851.962500 MHz	Concluding Recorded Call - Last Update: 4s	Recorder last write:4.72949	Call Elapsed: 12"
-        match = re.match(log_pattern, line)
-        # Ok, we have a first stage match.  This gets the date, loglevel, system, callnumber, talkgroup, frequency and
-        # everything else to parse. Then we will regex again to pull data from everything else.
-        if match:
+        if match := re.match(log_pattern, line):
             calldata = match[7]
             # Technically this isn't a real unixtimestamp as it's not timezone aware,
             # but we're just using it to create a unique index identifier.
             calldate = datetime.datetime.strptime(match[1], "%Y-%m-%d %H:%M:%S.%f")
             callts = calldate.timestamp()
             # Index for the dict is timestamp(ish)-talkgroup
-            callindex = str(int(callts)) + "-" + match[5]
+            callindex = f"{int(callts)}-" + match[5]
 
             # Second round of regexp.  Now we are going to harvest data from calldata - the "everything else"
             # Yes, this does need to be refactored as a loop ...
             # Note to me, make the regexp a list, for loop it, and change continue to break
             # Not recording talkgroup. Priority is -1.
             data_pattern = r".*(Not recording talkgroup.).*"
-            datamatch = re.match(data_pattern, calldata)
-            if datamatch:
+            if datamatch := re.match(data_pattern, calldata):
                 calldict[callindex] = {"excluded": True}
                 calldict[callindex].update(
                     {
@@ -51,8 +45,7 @@ def parselog():
                 continue
             # Not Recording: ENCRYPTED - src: 3290217
             data_pattern = r".*(ENCRYPTED).*"
-            datamatch = re.match(data_pattern, calldata)
-            if datamatch:
+            if datamatch := re.match(data_pattern, calldata):
                 calldict[callindex] = {"encrypted": True}
                 calldict[callindex].update(
                     {
@@ -67,8 +60,7 @@ def parselog():
                 continue
             # Not Recording: TG not in Talkgroup File
             data_pattern = r".*(TG not in Talkgroup File).*"
-            datamatch = re.match(data_pattern, calldata)
-            if datamatch:
+            if datamatch := re.match(data_pattern, calldata):
                 calldict[callindex] = {"unknown_talkgroup": True}
                 calldict[callindex].update(
                     {
@@ -83,8 +75,7 @@ def parselog():
                 continue
             # Concluding Recorded Call - Last Update: 4s	Recorder last write:4.4819	Call Elapsed: 7
             data_pattern = r".*Call Elapsed:\s+(\d+)"
-            datamatch = re.match(data_pattern, calldata)
-            if datamatch:
+            if datamatch := re.match(data_pattern, calldata):
                 calldict[callindex] = {"duration": datamatch[1]}
                 calldict[callindex].update(
                     {
@@ -96,7 +87,6 @@ def parselog():
                         "frequency": match[6],
                     }
                 )
-                continue
     logfile.close()
     return calldict
 
